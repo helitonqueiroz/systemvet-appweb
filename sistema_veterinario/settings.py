8cb3ba9bebc6d0650966fd77b9b601996680125d
helitonqueiroz <heliton.queiroz10@gmail.com>
2025-01-24 11:59:23 -0300

    Adicionando o domínio Render ao ALLOWED_HOSTS

--- sistema_veterinario/settings.py
+++ sistema_veterinario/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-lfp==a9o!_(t#^rguj^96^l3kg+l%u_q5x+(usk4pi4&4zju(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'systemvet-appweb.onrender.com']
 
 
 # Application definition
