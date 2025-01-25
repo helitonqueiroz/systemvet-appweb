462e894a5e53b1e6d8c1b9c983ef398261d4f5e2
helitonqueiroz <heliton.queiroz10@gmail.com>
2025-01-25 08:13:25 -0300

    Remoção da sidebar

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,1 @@
-// Abrir e fechar o menu lateral
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('menu-btn').addEventListener('click', function() {
-        document.getElementById('sidebar').classList.add('open');
-    });
-
-    document.getElementById('close-btn').addEventListener('click', function() {
-        document.getElementById('sidebar').classList.remove('open');
-    });
-});
-console.log("JavaScript carregado!");
-
+// Sidebar-related JavaScript removed
